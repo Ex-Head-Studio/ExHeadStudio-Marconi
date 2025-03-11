a65b54fa068c76b4816b82446fa7c703571122fd
stefanocraveia <stefano.craveia@gmail.com>
2025-03-11 18:06:48 +0100

    [UPDATE] Modificato test prefab navi; [UPDATE] Inserita logica bottoni main menu

--- Assets/Scripts/UI/MainMenu/ButtonsScript.cs
+++ Assets/Scripts/UI/MainMenu/ButtonsScript.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonsScript : MonoBehaviour
 {
+
+    /// <summary>
+    /// Lo script si occupa di gestire i pulsanti del menu principale, viene associato al canvas parent
+    /// </summary>
+    [Header("Main Menu Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject optionsPanel;
+
+    private void Awake()
+    {
+        mainMenuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(1).name);
     }
 
     public void ExitGame()
@@ -14,16 +29,23 @@ public class ButtonsScript : MonoBehaviour
 
     public void OpenCredits()
     {
-
+        creditsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(false);
     }
 
     public void OpensOptions()
-    {
+    {   
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(false);
 
     }
 
     public void BackToMainMenu()
     {
-        
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
     }
 }
