f42c4642ddb8d7b722736da54ed0c197f1bfc4b5
stefanocraveia <stefano.craveia@gmail.com>
2025-03-15 12:03:54 +0100

    [UPDATE] Commit prima della build di fine jam

--- Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
@@ -25,7 +25,7 @@ public class MainMenuButtons : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetSceneByBuildIndex(1).name);
+        SceneManager.LoadScene("GameScene");
         AudioManager.PlayOneShot(startButtonSound, this.transform.position);
     }
 
