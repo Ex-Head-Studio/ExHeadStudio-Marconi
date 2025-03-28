ec977936102ea877c59b43e20185e5e3da79a0d6
s312357 <robertofiore2712@gmail.com>
2025-03-28 01:13:51 +0100

    Demo Tutorial

---
+++ Assets/Scripts/TestUI/Tutorial.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using TMPro;
+
+public class TutorialManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI tutorialText;
+    public string[] tutorialMessages;
+    private int currentMessageIndex = 0;
+    public float Speed;
+    private Coroutine typingCoroutine; 
+    private bool isTyping = false; 
+    public GameObject backgroundPanel;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextSentence();
+        }
+    }
+
+    void NextSentence()
+    {
+        if (isTyping)
+        {
+            // Funzione che permette di fermare la coroutine e finire completamente la frase cliccando spazio mentre si sta scrivendo
+            StopCoroutine(typingCoroutine);
+            tutorialText.text = tutorialMessages[currentMessageIndex];
+            currentMessageIndex++; 
+            isTyping = false;
+        }
+        else if (currentMessageIndex < tutorialMessages.Length)
+        {
+            tutorialText.text = "";
+            typingCoroutine = StartCoroutine(TypeSentence());
+            PerformActionBasedOnMessage(currentMessageIndex); // Serve per cambiare lo sfondo in base a quale messaggio è visualizzato
+        }
+        else
+        {
+            tutorialText.transform.parent.gameObject.SetActive(false); 
+        }
+    }
+
+    void PerformActionBasedOnMessage(int messageIndex)
+    {
+        RectTransform rectTransform = backgroundPanel.GetComponent<RectTransform>();
+
+        switch (messageIndex)
+        {
+            case 2:
+            case 3:
+                rectTransform.offsetMin = Vector2.zero; 
+                rectTransform.offsetMax = Vector2.zero; 
+                rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, -300); // Set top to 300
+                break;
+            case 4:
+            case 5:
+                rectTransform.offsetMin = Vector2.zero; 
+                rectTransform.offsetMax = Vector2.zero; 
+                rectTransform.offsetMin = new Vector2(215, rectTransform.offsetMin.y); // Set left to 215
+                break;
+            case 6:
+            case 7:
+                rectTransform.offsetMin = Vector2.zero; 
+                rectTransform.offsetMax = Vector2.zero; 
+                rectTransform.offsetMax = new Vector2(-215, rectTransform.offsetMax.y); // Set right to 215
+                break;
+            default:
+                rectTransform.offsetMin = Vector2.zero; 
+                rectTransform.offsetMax = Vector2.zero; 
+                break;
+        }
+    }
+
+    IEnumerator TypeSentence()
+    {
+        isTyping = true;
+        tutorialText.text = "";
+        foreach (char letter in tutorialMessages[currentMessageIndex].ToCharArray())
+        {
+            tutorialText.text += letter;
+            yield return new WaitForSeconds(Speed);
+        }
+        isTyping = false;
+        currentMessageIndex++;
+    }
+}
