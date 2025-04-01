f8bca04483807d4d09016737f7a69ff28c43ca4d
gGargiulo <gabriele.garg@gmail.com>
2025-04-01 22:48:21 +0200

    [NEW] Sound component sul prefab delle navi

---
+++ Assets/Scripts/Audio/ShipSounds.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ShipSounds: MonoBehaviour
+{
+    private FMOD.Studio.EventInstance shipDestroy;
+
+    public void PlayShipDestroy()
+    {
+        shipDestroy = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/ShipDestroy");
+        shipDestroy.start();
+        shipDestroy.release();
+    }
+
+    private FMOD.Studio.EventInstance shipMove;
+    public void PlayShipMove()
+    {
+        shipMove = FMODUnity.RuntimeManager.CreateInstance("event:/UI/ShipMove");
+        shipMove.start();
+        shipMove.release();
+    }
+    
+}
