0d7f5c6d0e9f0bcd97cb298f8bb86a359df91545
GABRIELEPC\gabri <gabrielesantonauni@gmail.com>
2025-04-04 19:13:39 +0200

    [UPDATE] Cambiato algoritmo di scelta posizione

--- Assets/Scripts/Influence Maps/InfluenceMap.cs
+++ Assets/Scripts/Influence Maps/InfluenceMap.cs
@@ -47,6 +47,27 @@ public class InfluenceMap
 	{
 		return _influences[x, y];
 	}
+
+	public float CalculateMoveValue(int x, int y, int moveRange, int atkRange){
+		int count=0;
+		float value=0f;
+		for(int i=x-moveRange; i<=x+moveRange; i++){
+			for(int j=y-moveRange; j<=y+moveRange;j++){
+				if(i>=0 && i<Height && j>=0 && j<Width){
+					//Verifica se intorno alla posizione sono presenti più nemici che alleati
+					value+=GetValue(i, j);
+					count++;
+				}
+			}
+		}
+		if(value!=0f)
+			value=value/count;
+		else value=0f;
+		
+		return value;
+
+
+	}
 	
 	public InfluenceMap(int size, float decay, float momentum)
 	{
