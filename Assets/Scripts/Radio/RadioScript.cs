2d8580b7fbae77fcbbb664676e7baa4c4451247f
gGargiulo <gabriele.garg@gmail.com>
2025-04-09 20:04:40 +0200

    [NEW] Sound radio più o meno funzionante, con clip e knob (non implementato)

--- Assets/Scripts/Radio/RadioScript.cs
+++ Assets/Scripts/Radio/RadioScript.cs
@@ -5,10 +5,22 @@ public class RadioScript : MonoBehaviour
     [SerializeField] private RadioData radioData;
 
     private int radioIndex = 0;
+    private FMOD.Studio.EventInstance currentRadioInstance;
+
     public void ChangeClip()
     {
-        radioIndex = radioIndex  % radioData.radioEventReferences.Length;
-        FMOD.Studio.EventInstance radioInstance = FMODUnity.RuntimeManager.CreateInstance(radioData.radioEventReferences[radioIndex]);
+        // Stop the current clip if it is playing
+        if (currentRadioInstance.isValid())
+        {
+            currentRadioInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            currentRadioInstance.release();
+        }
+
+        // Play the next radio clip
+        radioIndex = radioIndex % radioData.radioEventReferences.Length;
+        currentRadioInstance = FMODUnity.RuntimeManager.CreateInstance(radioData.radioEventReferences[radioIndex]);
         radioIndex++;
+        currentRadioInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        currentRadioInstance.start();
     }
 }
