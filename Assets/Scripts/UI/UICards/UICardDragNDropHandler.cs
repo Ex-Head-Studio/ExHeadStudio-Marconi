a03c77c22e311ced87e261a2de8a5ae81c92da82
stefanocraveia <stefano.craveia@gmail.com>
2025-05-16 11:01:50 +0200

    [UPDATE] Commit intermedio

--- Assets/Scripts/UI/UICards/UICardDragNDropHandler.cs
+++ Assets/Scripts/UI/UICards/UICardDragNDropHandler.cs
@@ -7,42 +7,42 @@ using Unity.VisualScripting;
 using DG.Tweening;
 public class UICardDragNDropHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-        public static event Action<AbstractCard> cardDroppedEvent;
-        public static event Action<GameObject> cardUsedEvent;
+    public static event Action<AbstractCard> cardDroppedEvent;
+    public static event Action<GameObject> cardUsedEvent;
 
-        public static event Action<AbstractCard> droppableCardSelectedEvent;
-        public static event Action<AbstractCard> droppableCardDeselectedEvent;
-        private Vector3 startCardDragPosition;
-        private Vector3 mousePos;
+    public static event Action<AbstractCard> droppableCardSelectedEvent;
+    public static event Action<AbstractCard> droppableCardDeselectedEvent;
+    private Vector3 startCardDragPosition;
+    private Vector3 mousePos;
 
-        [SerializeField] private float minCardOffesetFromCamera = 10f;
-        private AbstractCard cardScript;
-        private EnergySystem energySystem;
-        private Collider cardCollider;
+    private float minCardOffesetFromCamera = 60f;
+    private AbstractCard cardScript;
+    private EnergySystem energySystem;
+    private Collider cardCollider;
 
-        private GridManager gridManager;
+    private GridManager gridManager;
 
 
-        private void Start()
-        {
-            cardScript = GetComponent<AbstractCard>();
-            cardCollider = GetComponent<Collider>();
-            //molto importante, non modificare, evita che le navi debbano avere un rigidbody
-            cardCollider.providesContacts = true;
+    private void Start()
+    {
+        cardScript = GetComponent<AbstractCard>();
+        cardCollider = GetComponent<Collider>();
+        //molto importante, non modificare, evita che le navi debbano avere un rigidbody
+        cardCollider.providesContacts = true;
 
-            gridManager = FindFirstObjectByType<GridManager>();
-
-        }
+        gridManager = FindFirstObjectByType<GridManager>();
+        //  minCardOffesetFromCamera = Vector3.Distance(this.gameObject.transform.position, gridManager.transform.position);
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if(energySystem != null && energySystem.currentEnergy <cardScript.GetCardCost())
+        if (energySystem != null && energySystem.currentEnergy < cardScript.GetCardCost())
         {
             transform.DOShakePosition(0.5f, 0.1f, 10, 90, false, true);
             //cambiare il colore per un attimo
             return;
         }
-        if(cardScript.isWolrdInteractive())
+        if (cardScript.isWolrdInteractive())
         {
             startCardDragPosition = transform.position;
             transform.position = GetPointerPositionInWorldSpace();
@@ -52,7 +52,7 @@ public class UICardDragNDropHandler : MonoBehaviour, IPointerDownHandler, IPoint
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        if(cardScript.isWolrdInteractive())
+        if (cardScript.isWolrdInteractive())
         {
             int i = 0;
 
@@ -92,18 +92,18 @@ public class UICardDragNDropHandler : MonoBehaviour, IPointerDownHandler, IPoint
     {
         //bisogna tenere a mente le dimensioni della finestra. Gli assi dello schermo hanno origine in basso a sx
 
-        if(Input.mousePosition.y >= Screen.height/4)
+        if (Input.mousePosition.y >= Screen.height / 4)
         {
-            
+
             mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, minCardOffesetFromCamera);
         }
         else
         {
             mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, minCardOffesetFromCamera);
         }
-      
-      Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
-      return objPos;
+
+        Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+        return objPos;
     }
 
 
@@ -112,6 +112,7 @@ public class UICardDragNDropHandler : MonoBehaviour, IPointerDownHandler, IPoint
     {
         this.energySystem = energySystem;
     }
+
 
 
 }
