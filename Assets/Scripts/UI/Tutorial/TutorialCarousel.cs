3147962d274544a10889fdae77148c25b08d3917
Luca De Martino <lucadema22@gmail.com>
2025-06-26 12:59:44 +0200

    [UPDATE] tutorialCarousel
    aggiornamenti grafici

--- Assets/Scripts/UI/Tutorial/TutorialCarousel.cs
+++ Assets/Scripts/UI/Tutorial/TutorialCarousel.cs
@@ -4,14 +4,21 @@ using TMPro;
 
 public class TutorialCarousel : MonoBehaviour
 {
+    [Header("Tutorial Carousel Settings")]
     [SerializeField] private Image imageDisplay;    // The UI Image component to display the tutorial images
     [SerializeField] private Sprite[] tutorialImages; // Array of tutorial images to display
     [SerializeField] private Button nextButton; // Button to go to the next image
     [SerializeField] private Button previousButton; // Button to go to the previous image
+    [Header("Image Index Display")]
     [SerializeField] private TextMeshProUGUI imageIndexText; // Text to display the current image index
+    [Header("Progress Bar Settings")]
     [SerializeField] private Slider progressBar; // Slider to indicate progress through the images
     [SerializeField] private RectTransform submarineIndicator; // immagine sottomarino
     [SerializeField] private RectTransform progressBarFillArea; // il rect che contiene la barra
+    [SerializeField] private float floatAmplitude = 10f; // Ampiezza del movimento oscillatorio del sottomarino
+    [SerializeField] private float floatFrequency = 1f; // Frequenza del movimento oscillatorio del sottomarino
+    public bool isCarouselActive = true; // Flag to control carousel activation
+    private Vector2 subBasePosition; // posizione fissa di riferimento
 
     private int currentIndex = 0; // Current index of the displayed image
 
@@ -20,8 +27,21 @@ public class TutorialCarousel : MonoBehaviour
         //Imposta primo sprite e aggiungi listener ai bottoni
         UpdateImage();
 
+        if (submarineIndicator != null)
+            subBasePosition = submarineIndicator.anchoredPosition;
+
         nextButton.onClick.AddListener(NextImage);
         previousButton.onClick.AddListener(PreviousImage);
+    }
+
+    void Update()
+    {
+        if (isCarouselActive && submarineIndicator != null)
+        {
+            Vector2 pos = submarineIndicator.anchoredPosition;
+            pos.y = subBasePosition.y + Mathf.Sin(Time.time * floatFrequency * 2f * Mathf.PI) * floatAmplitude;
+            submarineIndicator.anchoredPosition = new Vector2(pos.x, pos.y);
+        }
     }
 
     void UpdateImage()
@@ -45,7 +65,7 @@ public class TutorialCarousel : MonoBehaviour
                 Vector2 pos = submarineIndicator.anchoredPosition;
                 pos.x = t * barWidth;
                 submarineIndicator.anchoredPosition = pos;
-            }
+            }            
         }
 
         // Abilita o disabilita i bottoni in base all'indice corrente
